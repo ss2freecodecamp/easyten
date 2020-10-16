030a0e5f696ceb13f9edcbf0828a77d6ad5d8823
Sadrak S <ss2freecodecamp@gmail.com>
2020-10-16 10:14:54 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+alert("PLEASE NOTE: We have a VERY STRICT, NO REFUNDS policy.  Please make sure you are willing to use this system before completing your purchase.  
+No refund will be approved once you complete this transaction");
